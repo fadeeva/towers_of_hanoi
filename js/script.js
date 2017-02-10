3d570cc5ae0514abc53d69ed278f177c5cc48e2b
Katya Fadeeva <katfadmail@gmail.com>
2017-02-10 22:31:37 +0200

    disks

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+var disks = {    
+    
+    blue : {
+        color: "#73cad4",
+        width: 32,
+        rodGap: 105
+    },
+    
+    purple: {
+        color: "#9a73d4",
+        width: 78,
+        rodGap: 83
+    },
+    
+    orange : {
+        color: "#d4b173",
+        width: 122,
+        rodGap: 61
+    },
+    
+    green : {
+        color : "#afe66e",
+        width: 166,
+        rodGap: 39
+    },
+    
+    pink : {
+        color: "#ed82b7",
+        width: 210,
+        rodGap: 17
+    },
+    
+}
+
+var level_0 = {
+    rod_1 : [disks.pink, disks.green, disks.orange, disks.purple, disks.blue],
+    rod_2 : [disks.pink, disks.green, disks.orange, disks.purple, disks.blue],
+    ros_3 : [disks.pink, disks.green, disks.orange, disks.purple, disks.blue]
+}
+
 window.onload = function() {
     var theCanvas = document.getElementById('canvas');
     if (theCanvas && theCanvas.getContext) {
@@ -26,7 +66,38 @@ window.onload = function() {
             ctx.fillRect(347, 86, 6, 186);
             ctx.fillRect(578, 86, 6, 186);
             
-            // disks
+            function drawDisks(oLevel) { 
+                oLevel = (typeof oLevel !== "object") ? {} : oLevel;
+                var height = 248;
+                var rod = [];
+                var rodGap = 0;
+                
+                for(var key in oLevel) {
+                    rod = oLevel[key];
+                    rodNumber = parseInt(key[4]);
+                    
+                    if(rod.length) {                       
+                        rod.forEach(function(disk, i, arr) {
+                            if(rodNumber == 1) {
+                                rodGap = disk.rodGap;
+                            } else if(rodNumber == 2) {
+                                rodGap = disk.rodGap * 3 + (disk.width - 12)
+                            } else if(rodNumber == 3){
+                                rodGap = disk.rodGap * 5 + (disk.width - 12) * 2
+                            }
+                            
+                            ctx.fillStyle = disk.color;
+                            ctx.fillRect(rodGap, height, disk.width, 24);
+                            height -= 24;
+                        });
+                    }
+                    
+                    height = 248;
+                }
+            }          
         }
     }
+    drawDisks(level_0)
 }
+
+
